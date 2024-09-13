c9ec1798f74804bde364db1945126da7e942ca28
Abhay Bharti <abhaybharti2123@gmail.com>
2024-09-14 00:57:35 +0530

    Dasboard uda diya

--- TOOL_APP/Frontend/tool-app/src/App.jsx
+++ TOOL_APP/Frontend/tool-app/src/App.jsx
@@ -3,7 +3,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import LandingPage from './pages/LandingPage';
-import Dashboard from './pages/Dashboard';
 
 function App() {
   const router = createBrowserRouter([
